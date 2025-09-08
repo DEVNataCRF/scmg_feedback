7275542073e1673a3b179309afd8a522cc426074
Natã Corrêa <156501430+DEVNataCRF@users.noreply.github.com>
2025-09-08 14:04:07 -0300

    chore: add tsconfig for jest

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,10 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 export default {
-  testEnvironment: "node",
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
   transform: {
-    "^.+\.tsx?$": ["ts-jest", { tsconfig: "backend/tsconfig.json" }],
+    '^.+\\.tsx?$': ['ts-jest', { tsconfig: './tsconfig.jest.json' }],
   },
   setupFiles: ['jest-localstorage-mock'],
+  testPathIgnorePatterns: ['<rootDir>/backend/'],
 };
